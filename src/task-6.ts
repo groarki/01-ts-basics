feca528202c8d3c67c590efebdd2d658650583d1
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2025-05-08 17:09:48 +0200

    fixed task 6

--- src/task-6.ts
+++ src/task-6.ts
@@ -3,6 +3,6 @@ console.log("-------------------task 6----------------------");
 function getFirstElement<T>(arr:T[]):T {
   return arr[0];
 }
-console.log(getFirstElement([1,2,3,4])); // 1
+console.log(getFirstElement<number>([1,2,3,4])); // 1
 console.log(getFirstElement<string>(["a", "b", "c"]));     // "a"
 console.log(getFirstElement<boolean>([true, false, true])); // true
